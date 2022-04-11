551e93bb652a33621d55d565e3429fd28cf2ea7c
Berat BAYRAM <berat.bayram-p@tubitak.gov.tr>
2022-04-11 22:10:02 +0300

    Week 6 is started

---
+++ week_6/src/main/java/com/example/arackiralamaportali/main.java
@@ -0,0 +1,17 @@
+package main.java.com.example.arackiralamaportali;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class main implements CommandLineRunner{
+    public main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
